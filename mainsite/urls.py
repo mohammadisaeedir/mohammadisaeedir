4a61a489f49015443f5d22371275000ee286fbd2
saeedm1988 <mohammadi.saeed1988@gmail.com>
2022-04-04 23:10:59 +0430

    usingtemplatevie

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('contactme/', views.ContactForm.as_view()),
-    path('thanks/', views.submitform),
+    path('thanks/', views.SubmitView.as_view()),
     path('popup/', views.popup),
+    path('contacts', views.Contacts.as_view()),
+    path('contacts/<int:id>', views.surl, name='cont'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,10 +1,23 @@
-from webbrowser import get
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import Contact
 from django.views import View
+from django.views.generic.base import TemplateView
+from .models import ContactForm as cf
 
 # Create your views here.
+
+# def contact_me(request):
+#     if request.method == 'POST':
+#         myform = Contact(request.POST)
+#         if myform.is_valid():
+#             myform.save()
+#             return HttpResponseRedirect("/thanks")
+#     else:
+#         myform = Contact()
+#     return render(request, 'mainsite/contactme.html', {
+#         'form': myform,
+#     })
 
 
 class ContactForm(View):
@@ -32,18 +45,30 @@ def index(request):
     return render(request, 'mainsite/index.html')
 
 
-# def contact_me(request):
-#     if request.method == 'POST':
-#         myform = Contact(request.POST)
-#         if myform.is_valid():
-#             myform.save()
-#             return HttpResponseRedirect("/thanks")
-#     else:
-#         myform = Contact()
-#     return render(request, 'mainsite/contactme.html', {
-#         'form': myform,
-#     })
+# class SubmitView(View):
+#     def get(self, request):
+#         return render(request, 'mainsite/thanks.html')
+
+class SubmitView(TemplateView):
+    template_name = 'mainsite/thanks.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['body'] = 'we will be in touch'
+        return context
 
 
-def submitform(request):
-    return render(request, 'mainsite/thanks.html')
+class Contacts(TemplateView):
+    template_name = 'mainsite/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        contcats = cf.objects.all()
+        context["contacts"] = contcats
+        return context
+
+def surl(request, id):
+     obj = cf.objects.get(pk=id)
+     return render(request, 'mainsite/cont.html', {
+          'detail': obj
+     })
